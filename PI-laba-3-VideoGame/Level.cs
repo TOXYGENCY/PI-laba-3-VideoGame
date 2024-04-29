bb8a2f8c182828034ad4fbade1f31786fa731075
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-04-29 10:34:25 +0300

    Создал классы кроме VideoGame

---
+++ PI-laba-3-VideoGame/Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_laba_3_VideoGame
+{
+    public class Level
+    {
+        short LevelNumber;
+
+        /// <summary>
+        /// Конструктор класса Level
+        /// </summary>
+        /// <param name="LevelNumber">Число типа short</param>
+        public Level(short LevelNumber) 
+        {
+            this.LevelNumber = LevelNumber;
+        }
+
+    }
+}
---
+++ PI-laba-3-VideoGame/VideoGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_laba_3_VideoGame
+{
+    public class VideoGame<T>
+        where T : Game
+    {
+        
+    }
+}
